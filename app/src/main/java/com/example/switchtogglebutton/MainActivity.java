482997aec12866950c60895b8563b2deb3248c6c
Andriy <ak8022@bs.amalnet.k12.il>
2020-05-10 17:32:15 +0300

    Linking Components

--- app/src/main/java/com/example/switchtogglebutton/MainActivity.java
+++ app/src/main/java/com/example/switchtogglebutton/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.switchtogglebutton;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button b=(Button) findViewById(R.id.button);
+    Switch s=(Switch) findViewById(R.id.switch1);
+    ToggleButton tb=(ToggleButton) findViewById(R.id.toggleButton);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
