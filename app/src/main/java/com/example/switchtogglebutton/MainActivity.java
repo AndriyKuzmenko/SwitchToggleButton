e01741faf57d8c22f05045bbf4a36b0ef737d0a1
Andriy <ak8022@bs.amalnet.k12.il>
2020-05-10 17:39:35 +0300

    Reading switch and toggle button

--- app/src/main/java/com/example/switchtogglebutton/MainActivity.java
+++ app/src/main/java/com/example/switchtogglebutton/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity
 
     public void buttonPressed(View view)
     {
-        //
+        bo1=s.isChecked();
+        bo2=tb.isChecked();
     }
 }
